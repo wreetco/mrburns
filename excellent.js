b2f7245a25482186479660e885f1ec4142d19fb8
Chase Hagganz <chasehx@gmail.com>
2016-05-17 19:59:20 -0600

    adding a basic logger

--- excellent.js
+++ excellent.js
@@ -5,6 +5,7 @@ var express = require("express");
 var app = express();
 var methodOverride = require("method-override");
 var fs = require('fs');
+var log = require('./lib/wlog.js');
 
 // app information
 var app_info = {
