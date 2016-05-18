0c4f5f5b1140870bfef03b20eb8bd2344045a2d9
Chase Hagganz <chasehx@gmail.com>
2016-05-17 18:40:47 -0600

    fix indenting

--- routes.js
+++ routes.js
@@ -7,69 +7,69 @@ var UserCtrl = require("./app/controllers/user_ctrl.js").UserCtrl;
 var ReminderCtrl = require("./app/controllers/reminder_ctrl.js").ReminderCtrl;
 
 module.exports = function(app) {
-// example model route
-app.get('/example/show', function(req, res) {
-	examples.show(req, res);
-});
+  // example model route
+  app.get('/example/show', function(req, res) {
+    examples.show(req, res);
+  });
 
-/*
-  * Account actions
-*/
-// GET /account
-app.get('/account', function(req, res) {
-  res.send(AccountCtrl.demo());
-});
-// end get /account
-// end account actions
+  /*
+    * Account actions
+  */
+  // GET /account
+  app.get('/account', function(req, res) {
+    res.send(AccountCtrl.demo());
+  });
+  // end get /account
+  // end account actions
 
-/*
-  * Tag actions
-*/
-// GET /tag
-app.get('/tag', function(req, res) {
-  res.send(TagCtrl.demo());
-});
-// end get /tag
-// end tag actions
+  /*
+    * Tag actions
+  */
+  // GET /tag
+  app.get('/tag', function(req, res) {
+    res.send(TagCtrl.demo());
+  });
+  // end get /tag
+  // end tag actions
 
-/*
-  * Contact actions
-*/
-// GET /contact
-app.get('/contact', function(req, res) {
-  res.send(ContactCtrl.demo());
-});
-// end get /contact
-// end contact actions
+  /*
+    * Contact actions
+  */
+  // GET /contact
+  app.get('/contact', function(req, res) {
+    res.send(ContactCtrl.demo());
+  });
+  // end get /contact
+  // end contact actions
 
-/*
-  * Field actions
-*/
-// GET /field
-app.get('/field', function(req, res) {
-  res.send(FieldCtrl.demo());
-});
-// end get /field
-// end field actions
+  /*
+    * Field actions
+  */
+  // GET /field
+  app.get('/field', function(req, res) {
+    res.send(FieldCtrl.demo());
+  });
+  // end get /field
+  // end field actions
 
-/*
-  * User actions
-*/
-// GET /user
-app.get('/user', function(req, res) {
-  res.send(UserCtrl.demo());
-});
-// end get /user
-// end user actions
+  /*
+    * User actions
+  */
+  // GET /user
+  app.get('/user', function(req, res) {
+    res.send(UserCtrl.demo());
+  });
+  // end get /user
+  // end user actions
 
-/*
-  * Reminder actions
-*/
-// GET /reminder
-app.get('/reminder', function(req, res) {
-  res.send(ReminderCtrl.demo());
-});
-// end get /reminder
-// end reminder actions
+  /*
+    * Reminder actions
+  */
+  // GET /reminder
+  app.get('/reminder', function(req, res) {
+    res.send(ReminderCtrl.demo());
+  });
+  // end get /reminder
+  // end reminder actions
 
 };
