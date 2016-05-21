9c8a3490794808423b4cebdd3b0838724e4434b8
Chase Hagganz <chasehx@gmail.com>
2016-05-21 14:41:16 -0600

    fix some relations on manager

--- app/models/manager.js
+++ app/models/manager.js
@@ -5,10 +5,10 @@ var Schema = mongoose.Schema;
 var manager_schema = mongoose.Schema({
   organization: String,
   account: {type: Schema.ObjectId, ref: 'Account'},
-  modules: {type: Schema.ObjectId, ref: 'Module'},
+  modules: [{type: Schema.ObjectId, ref: 'Module'}],
   contacts: [{type: Schema.ObjectId, ref: 'Contact'}],
   users: [{type: Schema.ObjectId, ref: 'User'}],
-  custom_fields: {type: Schema.ObjectId, ref: 'Field'},
+  custom_fields: [{type: Schema.ObjectId, ref: 'Field'}],
 }, {
   timestamps: {
     createdAt: "created_date",
