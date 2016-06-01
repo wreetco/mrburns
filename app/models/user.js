1c5cd4b23213a46efdd5cacb2e48ff4b484070b4
Chase Hagganz <chasehx@gmail.com>
2016-05-31 21:49:34 -0600

    rework POST /user a little, prepare for db-enforced email field uniqueness

--- app/models/user.js
+++ app/models/user.js
@@ -9,7 +9,7 @@ var Wregx = require('./../../lib/wregx');
 var Schema = mongoose.Schema;
 
 var user_schema = mongoose.Schema({
-	email: String,
+	email: {type: String, required: true, index: true, unique: true},
 	password: String,
   account: {type: Schema.ObjectId, ref: 'Account'},
   managers: [{type: Schema.ObjectId, ref: 'Manager'}],
@@ -22,33 +22,30 @@ var user_schema = mongoose.Schema({
 });
 
 user_schema.methods.new = function(u) {
+  //new User({email:"lsdol@attack.com",password:"lol"}).save().then(function(res){user = res}, function(err){e = err})
   // add a new user to the database
-  var d = q.defer();
-  // first let's clean up the user
-  if (!Wregx.isEmail(u.email)) {
-    d.reject(Errors.invalidEmail());
-    return d.promise;
-  }
-  //else if (!Wregx.isHexstr(u.account)) {
-  //  d.reject(Errors.invalidId());
-    //return d.promise;
-  //}
-  // really the only things to clean, the process of hashing cleans password up
-  // let's make the new user and save it
-  // seems this is a place where our created_date hash hurts as and was not so clever
-  // as we will have to make two save calls, or race the save, neither great options
-  try {
+  return new Promise(function(resolve, reject) {
+    // first let's clean up the user
+    if (!Wregx.isEmail(u.email)) {
+      d.reject(Errors.invalidEmail());
+      return d.promise;
+    }
+    // really the only things to clean, the process of hashing cleans password up
+    // let's make the new user and save it
+    // seems this is a place where our created_date hash hurts as and was not so clever
+    // as we will have to make two save calls, or race the save, neither great options
     new User(u).save().then(function(user) {
       // now hash and re-save, sigh
       user.password = user.hashPassword(u.password);
       user.save().then(function(user) {
-        d.resolve(user);
+        resolve(user);
+      }, function(e) {
+        reject(Errors.saveError());
       });
+    }, function (e) {
+      reject(Errors.saveError());
     });
-  } catch(e) {
-    d.reject(e);
-  }
-  return d.promise;
+  }); // end promise
 };
 
 user_schema.methods.hashPassword = function(passwd) {
@@ -58,15 +55,14 @@ user_schema.methods.hashPassword = function(passwd) {
 };
 
 user_schema.methods.getByEmail = function() {
-  var d = q.defer();
-  User.findOne({email: this.email}, function(err, u) {
-    if (u) {
-      d.resolve(u);
-    }
-    else
-      d.resolve(false);
-  });
-  return d.promise;
+  return new Promise(function(resolve, reject) {
+    User.findOne({email: this.email}, function(err, u) {
+      if (u)
+        resolve(u);
+      else
+        reject(Errors.noMatch());
+    });
+  }); // end promise
 };
 
 // statics
